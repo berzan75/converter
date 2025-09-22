d07e0f2f597dc178693c4ccca9623c3e7caeb3a5
berzan75 <issundee358@gmail.com>
2025-09-22 19:33:28 +0200

    Update home.py

--- home.py
+++ home.py
@@ -6,12 +6,12 @@ top.geometry("600x400")
 top.config(background="light blue")
 
 def convort():
-    temp_cel=float(p.get())
     try:
+        temp_cel=float(p.get())
         fahrenheit=(temp_cel*9/5)+32
         aswer.config(text=f"Temprature In Fahrenheit:{fahrenheit:.1f}")
     except:
-        pass
+        aswer.config(text="please enter a valid number")
 
 
 
@@ -19,16 +19,20 @@ text = Label(top,
                 text="Celsius --> Fahrenheit",font=("Times New Roman",30)).place(x=150,
                                         y=10)
 enter= Label(top,
-                     text="Enter Temperature in Celsuis ",font=("Times New Roman",15),bd='3').place(x=40,
+                     text="Enter Temperature in Celsius ",font=("Times New Roman",15),bd='3')
+enter.place(x=30,
                                             y=140)
 aswer=Label(top,
-        text="Temperature In Fahrenheit Is:",font=("Times New Roman",15),bg="light blue").place(x=80,
+        text="Temperature In Fahrenheit Is:",font=("Times New Roman",15),bg="light blue")
+aswer.place(x=80,
                                                                                 y=180)
 
 p= Entry(top,
-                width=20,font=("Arial",18)).place(x=280,
-                                y=140)
+                width=20,font=("Arial",18))
+p.place(x=280, y=140)
+
 submit=Button(top,
-              text="Convert",font=("Arial",20),command=convort).place(x=200,
-                                   y=250)
+              text="Convert",font=("Arial",20),command=convort)
+submit.place(x=200, y=250)
+
 top.mainloop()
